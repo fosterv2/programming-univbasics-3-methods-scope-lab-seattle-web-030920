bcf563f59accae7c8b63864c643b9e49e2382f60
Valerie Foster <fosterv2@outlook.com>
2020-01-10 01:46:40 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,15 @@
+$phrase = "Do A Barrel Roll!"
+
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+end
+
+def any_phrase(phrase)
+  puts phrase
 end
